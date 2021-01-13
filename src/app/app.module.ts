0da068398575933c9ab5e9d9a48d7fa549002f77
vaishnavi <vaishnavip1012@gmail.com>
2021-01-13 17:41:31 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormComponent,
+    NavBarComponent,
+    EmployeeDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path: '', component: ReactiveFormComponent},
+      { path: 'reactive-form', component: ReactiveFormComponent },
+
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/customvalidation.service.ts
@@ -0,0 +1,92 @@
+import { invalid } from '@angular/compiler/src/render3/view/util';
+import { Injectable } from '@angular/core';
+import { ValidatorFn, AbstractControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { controlNameBinding } from '@angular/forms/src/directives/reactive_directives/form_control_name';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomvalidationService {
+  patternValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (!control.value) {
+        return null;
+      }
+      const regex = new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$');
+      const valid = regex.test(control.value);
+      return valid ? null : { invalidPassword: true };
+    };
+  }
+  MatchPassword(password: string, confirmPassword: string) {
+    return (formGroup: FormGroup) => {
+      const passwordControl = formGroup.controls[password];
+      const confirmPasswordControl = formGroup.controls[confirmPassword];
+      if (!passwordControl || !confirmPasswordControl) {
+        return null;
+      }
+      if (confirmPasswordControl.errors && !confirmPasswordControl.errors.passwordMismatch) {
+        return null;
+      }
+      if (passwordControl.value !== confirmPasswordControl.value) {
+        confirmPasswordControl.setErrors({ passwordMismatch: true });
+      } else {
+        confirmPasswordControl.setErrors(null);
+      }
+    }
+  }
+  userNameValidator(userControl: AbstractControl) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        if (this.validateUserName(userControl.value)) {
+          resolve({ userNameNotAvailable: true });
+        } else {
+          resolve(null);
+        }
+      }, 1000);
+    });
+  }
+  validateUserName(userName: string) {
+    const UserList = ['ankit', 'admin', 'user', 'superuser'];
+    return (UserList.indexOf(userName) > -1);
+  }
+
+  validateId(): ValidatorFn{
+    return (control: AbstractControl): { [key: string]: any } => {
+      if(!control.value){
+        return null;
+      }
+
+      const regex = new RegExp('/H[I]N/g');
+      const valid = regex.test(control.value);
+      return valid ? null: { invalidempId: true };
+    }
+  }
+
+  validateNumber(): ValidatorFn{
+    return (control: AbstractControl): { [key: string]: any } => {
+      if(!control.value){
+        return null;
+      }
+      const regex = new RegExp('^([1-9]{1})([01234789]{1})([0-9]{8})$');
+      const valid = regex.test(control.value);
+      return valid ? null: { invalidNumber: true };
+
+    }
+  }
+   validateMail(): ValidatorFn{
+     return (control: AbstractControl): {[key: string]: any } => {
+       if(!control.value){
+         return null;
+       }
+     
+     const regex = new RegExp('^[a-z0-9](\.?[a-z0-9]){5,}@customfurnish\.com$');
+     const valid = regex.test(control.value);
+     return valid ? null: { invalidMail: true };
+    }
+   }
+  
+   
+   
+
+}
